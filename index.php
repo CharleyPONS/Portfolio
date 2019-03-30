d91b57a14a5c7831998831176b0e17202b8e2b1d
C.pons <charleypons@live.fr>
2019-03-30 06:05:58 +0100

    error corrected

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="fr">
 <head>
   <meta charset="utf-8" />
@@ -21,15 +22,15 @@
   <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet">
   <!-- C-pons -->
   <link rel="icon" type="image/jpg" href="Images/icone.jpg" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" />
   <!-- Jquery -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"> </script>
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <!-- Optional theme -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <!-- C-pons -->
   <link rel="stylesheet" href="css/style.css" />
 
@@ -37,74 +38,366 @@
 </head>
 <body>
   <img class="backdev" src="Images/background.jpg" alt="">
-  <header>   
+  <header id="home_anchor" >   
 
-   <nav class="navbar navbar-expand-lg navbar-default navbar-fixed-top ">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="true">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div class="brand_nav">
-          <a class="navbar-brand" href="#">Charley Pons</a>
-          
-        </div>
-      </div>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+    <a class="navbar-brand" href="#">Charley Pons</a>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="navbar-collapse-1">
-        <ul class="nav navbar-nav mr-auto">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-          <li class="active"><a href="#home">Acceuil</a></li>
-          <li><a href="#b">Mon profil</a></li>
-          <li><a href="#c">Compétences/Certifications</a></li>
-          <li><a href="#d">Réalisations</a></li>
-          <li role="separator" class="divider"></li>
-          <li><a href="#">Contactez-moi</a></li>
-        </ul>
-      </li>
-    </ul>
 
-    <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Contact<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#"><span class="fa fa-phone"></span>  06 81 46 31 67</a></li>
-          <li><a href="mailto:charleypons@live.fr"><span class="fas fa-envelope"></span>  Mail</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div><!-- /.navbar-collapse -->
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1" >
+      <ul class="nav-active-tab navbar-nav nav-pills mr-auto">
 
-</div><!-- /.container-fluid -->
-</nav>
+        <li class="nav-item"><a class="nav-link active" href="#home_anchor">Aceuil</a></li>
+        <li class="nav-item"><a class="nav-link" href="#profil">Mon profil</a></li>
+        <li class="nav-item"><a class="nav-link" href="#skills">Compétences</a></li>
+        <li class="nav-item"><a class="nav-link" href="#portfolio">Réalisations</a></li>
+        <li class="nav-item"><a class="nav-link" href="#contact">Contactez-moi</a></li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mes corrdonnées</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#"><span class="fa fa-phone"></span>06 81 46 31 67</a>
+            <a class="dropdown-item" href="mailto:charleypons@live.fr"><span class="fas fa-envelope"></span>Mail</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
 </header>
 
 <section id="home">
-  <div class="container-fluid homePage">
-    <div class="text_Home_Page">
+  <div class="text_Home_Page">
+    <div>
       <h1>BONJOUR! je m'appelle <span>Charley</span></h1>
       <h3><span>Passionné</span> par les nouvelles technologies, je me suis formé au métier de <span>Développeur Web</span> et je suis</h3>
       <h3>disponible pour relever de nouveaux challenges.</h3>
     </div>
     <div class="button_Home">
-        <a class="btn btn-default" role="button" href="CV_PONS_CHARLEY.pdf" target="_blank"><span class="fas fa-download"></span> CURRICULUM VITAE</a>    
-        <a class="btn btn-default"role="button"  href="https://github.com/CharleyPONS/"  target="_blank"><span class="fab fa-github"></span> Mon compte GITHub</a>
-        <a class="btn btn-default" role="button" href=""><span class="fas fa-envelope"  target="_blank"></span> Contactez-moi</a>
+      <a class="btn btn-outline-success" role="button" href="CV_PONS_CHARLEY.pdf" target="_blank"><span class="fas fa-download"></span> CURRICULUM VITAE</a>    
+      <a class="btn btn-outline-success"role="button"  href="https://github.com/CharleyPONS/"  target="_blank"><span class="fab fa-github"></span> Mon compte GITHub</a>
+      <a class="btn btn-outline-success" role="button" href="#contact"><span class="fas fa-envelope"  target="_blank"></span> Contactez-moi</a>
     </div>
-
   </div>
+
 </section>
 
+<section id="profil">
 
-</body>
+  <article>
+    <div class="myPresentation">
+      <h1 class="titleSection">Un peu de moi !</h1>
+
+      <p>Bonjour, je suis un jeune développeur, passionné par la programmation en général et particulièrement ce qui touche à la programmation de site web!</p>
+
+      <p>Curieux et autodidacte, j'ai étudié les langages <strong>HTML</strong>, <strong>CSS</strong>, <strong>PHP</strong>.</p>
+
+      <p>J'ai également étudié le <strong>Javascript</strong> et <strong>Symfony</strong> dans sa <i>3<sup>éme</sup> version</i> pendant ma reconversion de "<strong>Développeur Web</strong>".</p>
+
+      <p>Après avoir suivi une formation de "<strong>Développeur intégrateur en réalisation d’applications web</strong>" je recherche désormais un poste en<strong> alternance</strong> de Développeur Web, afin de  préparer un BAC+ 3 « Développeur Full-Stack » dans un domaine qui me passionne.</p>
+
+
+      <p>N'hésitez pas à me contacter si vous souhaitez en savoir plus sur mes compétences et ma motivation.</p>
+
+    </div>
+  </article>
+
+  <article><img src="Images/photo.png"></article>
+
+  <article>
+    <div class="personalInformation">
+      <h1 class="titleSection">Informations Personnelles</h1>
+
+      <ul>
+        <li><span>Nom : </span>Charley PONS</li>
+        <li><span>Age : </span>27 ans</li>
+        <li><span>Téléphone : </span>+33 6 81 46 31 67</li>
+        <li><span>Email : </span>charleypons@live.fr</li>
+        <li><span>Adresse : </span>le Pecq 78230</li>
+      </ul>
+
+    </article>
+
+  </section>
+
+
+  <section id="skills">
+
+    <h1 class="titleSection">Compétences</h1>
+
+    <p class="introSection">Voici les compétences que j'ai acquises au cours de mon expérience en autodidacte et de ma formation de <strong>Développeur Web</strong>.</p>
+
+    <article>
+
+      <div class="blocSkill">
+
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-html5 html"></span> HTML</h1>
+          <p>Langage indispensable lorsque l'on souhaite créer des sites web. L'ergonomie est la chose que je tente de respecter au plus lors de la conception de mes pages web.</p>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+            <span class="sr-only">90% Complete</span>
+            90%
+
+          </div>
+        </div>
+
+      </div>
+
+
+
+      <div class="blocSkill">
+
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-css3-alt css"></span> CSS</h1>
+          <p>Langage complémentaire du HTML pour l'embellisement des page web. J'utilise les dernières règles CSS compatibles avec la majorité des navigateurs du marché.</p>
+
+          <ul>
+            <li><strong>SASS</strong></li>
+          </ul>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 85%">
+            <span class="sr-only">85% Complete</span>
+            85%
+
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="blocSkill">
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-php php"></span> PHP</h1>
+
+          <p>Langage de programmation libre principalement utilisé pour produire des pages Web dynamiques. Il est le langage orienté back-end que j'apprécie le plus.</p>
+
+          <ul>
+            <li><strong>Maîtrise de la POO de base</strong></li>
+            <li><strong>Architecture MVC</strong></li>
+          </ul>
+        </div>
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+            <span class="sr-only">70% Complete</span>
+            70%
+
+          </div>
+        </div>
+      </div>
+
+      <div class="blocSkill">
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-symfony symf"></span> SYMFONY</h1>
+
+          <p>Framework MVC le plus populaire. Ma première expérience sur ce Framework m'a beaucoup appris.</p>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+            <span class="sr-only">60% Complete</span>
+            60%
+
+          </div>
+        </div>
+
+      </div>
+
+      <div class="blocSkill">
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-bootstrap boot"></span> BOOTSTRAP</h1>
+
+          <p>Framework HTML, CSS, JS le plus populaire. L'utilisation de ce framework très puissant m'a permis d'améliorer le design de mes sites et la gestion du "responsive".</p>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 85%">
+            <span class="sr-only">85% Complete</span>
+            85%
+
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="blocSkill">
+        <div class="knowledgeSkill">
+          <h1><span class="fab fa-js js"></span> JavaScript</h1>
+
+          <p>Langage de programmation principalement employé pour la création de pages web interactive. Mon expérience sur ce langage fut riche en connaissances.</p>
+
+          <ul>
+            <li><strong>Jquery</strong></li>
+            <li><strong>POO Class ES6</strong></li>
+
+          </ul>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+            <span class="sr-only">70% Complete</span>
+            70%
+
+          </div>
+        </div>
+
+      </div>
+
+    </article>
+
+    <section id="portfolio">
+
+
+      <h1 class="titleSection">Mes Réalisations</h1>
+
+      <p class="introSection">Voici les différents projets personnels qui m'ont permis l'obtention de mon diplôme de <strong>"Développeur intégrateur en réalisation d’applications web".</strong></p>
+
+      <div class="portfolio_Project">
+
+        <div class="Main_box">
+          <h2><span class="fas fa-lightbulb"></span> Projet 1</h2><h3>Intégrez la maquette du site d'une agence web</h3>
+          <div class="portfolio_box">
+            <a href="http://www.web-agency-france.fr/" target="_blank">
+              <img src="Images/Projet1.png" alt="">
+              <figcaption class="description_Portfolio">
+                <h2>Lien disponible, cliquez ici!</h2>
+
+              </figcaption>
+            </a>
+          </div>
+        </div>
+
+        <div class="Main_box">
+          <h2><span class="fas fa-lightbulb"></span> Projet 2</h2><h3>Créez un thème WordPress</h3>
+          <div class="portfolio_box">
+            <a href="http://www.charleypons-strasbourg.fr/" target="_blank">
+              <img src="Images/Projet2.png" alt="">
+              <figcaption class="description_Portfolio">
+                <h2>Lien disponible, cliquez ici!</h2>
+
+              </figcaption>
+            </a>
+          </div>
+        </div>
+
+        <div class="Main_box"> 
+          <h2><span class="fas fa-lightbulb"></span> Projet 3</h2><h3>Carte interactive de location de vélos</h3>
+          <div class="portfolio_box">
+            <a href="http://xn--charley-pons-vlo-lyon-o5b.fr/" target="_blank">
+              <img src="Images/Projet3.png" alt="">
+              <figcaption class="description_Portfolio">
+                <h2>Lien disponible, cliquez ici!</h2>
+              </figcaption>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="Main_box"><h2><span class="fas fa-lightbulb"></span> Projet 4</h2><h3>Créez un blog pour un écrivain</h3>
+          <div class="portfolio_box">
+            <a href="">
+             <img src="Images/construction.jpg" alt="">
+             <figcaption class="description_Portfolio">
+              <h2>Lien bientôt disponible!</h2>
+            </figcaption>
+          </a>
+
+        </div>
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="contact">
+
+
+    <h1 class="titleSection">Contact</h1>
+
+    <p class="introSection">Vous souhaitez me proposer un poste ou avoir plus de renseignement sur mes compétences, vous pouvez me contacter via ce formulaire de contact</p>
+
+    <form class="form-group" action="index.php" method="post">
+      <div class="container-fluid contact_Form">
+
+        <div class="input-group mb-3">
+          <div class="offset-md-3 col-md-2">
+
+            <input class="form-control" type="text" placeholder=" Name" name="name" id="name" required/>
+          </div>
+
+          <div class="col-md-2">
+            <input class="form-control" type="email" placeholder=" Email" name="email" id="email" required/>
+          </div>
+
+          <div class="col-md-2">
+            <input class="form-control" type="text" placeholder=" Subject" name="subject" id="subject" required/>
+          </div>
+        </div>
+
+        <div class="input-group mb-2">
+          <div class=" offset-md-4 col-md-4 col-sd-3">                   
+           <textarea class="textarea_message form-control" rows="4" name="message" id="message" placeholder="Message" required></textarea> 
+         </div>
+       </div>
+
+       <div class="input-group mb-3">
+        <div class="offset-md-4 col-md-4">
+          <button type="submit" class="btn btn-success btn-lg btn-block">Envoyer</button>
+        </div>
+      </div>
+    </div>
+
+  </form>
+  <?php
+
+  function chargerClasse($classname)
+  {
+    require 'php/class/' . $classname . '.php';
+  }
+  spl_autoload_register('chargerClasse');
+
+  $db = new PDO('mysql:host=localhost;dbname=portfolio','CharleyPONS', 'Azeres12!!!!');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+  $manager = new ContactManager($db);
+  if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']))
+  {
+    $customer = new Contact([
+      'Name' => $_POST['name'],
+      'email' => $_POST['email'],
+      'Subject' => $_POST['subject'],
+      'Message' => $_POST['message'],
+    ]);
+
+    $manager->add($customer);
+
+    echo "<script>
+    alert('Votre message a bien été envoyé');</script>";
+
+  };
+
+
+
+
+  ?>
+</section>
 
 
 <!-- Libraries -->
 
 <script type="text/javascript" src="js/menu.js"></script>
 <script src="js/resize.js"></script>
+
+</body>
+
